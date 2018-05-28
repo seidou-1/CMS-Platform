f303839d21a62f7eea526b3dc92b5d547597d08f
Ashley DeMadet <darthvader@Ashleys-MacBook-Pro.local>
2018-05-28 13:31:22 -0400

    SHINOBILIFE-23 makes categoryDaoImpl

--- BlogCMS/src/main/java/com/sg/blogcms/dao/CategoryDaoImpl.java
+++ BlogCMS/src/main/java/com/sg/blogcms/dao/CategoryDaoImpl.java
@@ -6,8 +6,14 @@
 package com.sg.blogcms.dao;
 
 import com.sg.blogcms.dto.Category;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -23,31 +29,79 @@ public class CategoryDaoImpl implements CategoryDAOInterface {
     
 //    ==============PREPARED STATEMENTS============
 
+    private static final String SQL_INSERT_CATEGORIES = 
+            "insert into Categories(CatergoryName) "
+            + " values (?)";
+    
+    private static final String SQL_DELETE_CATEGORIES =
+            "delete from Categories "
+            + "where CategoryId = ?";
+    
+    private static final String SQL_UPDATE_CATEGORIES = 
+            "update Categories "
+            + "set CategoryName = ?"
+            + " where CategoryId = ?";
+    
+    private static final String SQL_SELECT_CATEGORIES =
+            "select * from Categories"
+            + "where CategoryId = ?";
+    
+    private static final String SQL_SELECT_ALL_CATEGORIES = 
+            "select * from Categories";
+    
     
 //    ==========METHODS==============
     @Override
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public Category addCategory(Category category) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        jdbcTemplate.update(SQL_INSERT_CATEGORIES,
+        category.getCategoryName());
+        
+//        this gets queries the database for the new ID that was just set to the row
+        int newID = jdbcTemplate.queryForObject("select LAST_INSERT_ID()", Integer.class);        
+//        this gets that ID and sets it to the specic category
+        category.setCategoryId(newID);
+        return category;
     }
 
     @Override
     public Category updateCategory(Category category) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        jdbcTemplate.update(SQL_UPDATE_CATEGORIES,
+                category.getCategoryName(),
+                category.getCategoryId());
+        return category;
     }
 
     @Override
     public void deleteCategory(int categoryId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        jdbcTemplate.update(SQL_DELETE_CATEGORIES, categoryId);
     }
 
     @Override
     public Category getCategoryById(int categoryId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+            return jdbcTemplate.queryForObject(SQL_SELECT_CATEGORIES, new CategoryMapper(), categoryId);
+        }catch (EmptyResultDataAccessException ex){
+            return null;
+        }
     }
 
     @Override
     public List<Category> getAllCategories() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return jdbcTemplate.query(SQL_SELECT_ALL_CATEGORIES, new CategoryMapper());
+    }
+    
+//    ====================MAPPER=========
+    private static final class CategoryMapper implements RowMapper<Category>{
+        
+        @Override
+        public Category mapRow(ResultSet rs , int rowNum) throws SQLException{
+            Category category = new Category();
+            category.setCategoryId(rs.getInt("CategoryID"));
+            category.setCategoryName(rs.getString("CategoryName"));
+            return category;
+        }
+        
     }
 
 }
