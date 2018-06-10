fe7306346b1fef9bbc88bf922a45e7539025941d
Travon Campbell <travonlcampbell@gmail.com>
2018-06-10 15:01:40 -0400

    SHINOBILIFE-31 Security feature merge

--- BlogCMS/src/main/java/com/sg/blogcms/controller/UserController.java
+++ BlogCMS/src/main/java/com/sg/blogcms/controller/UserController.java
@@ -8,9 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.sg.blogcms.service.UserServiceInterface;
-import java.util.Map;
+import java.util.List;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
